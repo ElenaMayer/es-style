e266a36d5cd8ad4ab1164869de840d64effe8e66
Mayer E <mayer.elen@gmail.com>
2017-11-09 11:57:47 +0700

    Parser for socials

---
+++ protected/views/site/_parser.php
@@ -0,0 +1,2 @@
+<script src="https://grably-parser.ru/js/parser_widget.js" type="text/javascript"></script>
+<div id="grably-button" data-uuid="dbe49efd-8eef-5438-b0f6-d91d861814e6" data-position="position-right" data-color="#902b56" style="color: white"></div>
